52beed273f67020a04e334544779810e260be5ec
Alex Chapman <a.s.chaperzzz@gmail.com>
2016-03-13 03:18:09 +0000

    group project added

---
+++ Chat Client/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.listBoxChat = new System.Windows.Forms.ListBox();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your user name:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(517, 48);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Details";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(159, 15);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(352, 20);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonConnect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonConnect.Enabled = false;
+            this.buttonConnect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonConnect.Location = new System.Drawing.Point(126, 66);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(128, 25);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect!";
+            this.buttonConnect.UseCompatibleTextRendering = true;
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // listBoxChat
+            // 
+            this.listBoxChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxChat.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBoxChat.FormattingEnabled = true;
+            this.listBoxChat.ItemHeight = 16;
+            this.listBoxChat.Location = new System.Drawing.Point(12, 97);
+            this.listBoxChat.Name = "listBoxChat";
+            this.listBoxChat.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBoxChat.Size = new System.Drawing.Size(517, 331);
+            this.listBoxChat.TabIndex = 3;
+            this.listBoxChat.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBoxChat_DrawItem);
+            this.listBoxChat.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.listBoxChat_MeasureItem);
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.AcceptsReturn = true;
+            this.textBoxMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMsg.Location = new System.Drawing.Point(12, 434);
+            this.textBoxMsg.Multiline = true;
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(517, 88);
+            this.textBoxMsg.TabIndex = 4;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSend.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSend.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSend.Location = new System.Drawing.Point(126, 528);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(128, 36);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = false;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 576);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.listBoxChat);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Pc Part Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ListBox listBoxChat;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ Chat Server/Chat Server/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chat_Server
+{
+    class Program
+    {
+        public static Hashtable clientsList = new Hashtable(); 
+
+        static void Main(string[] args)
+        {
+
+            TcpListener serverSocket = new TcpListener(8888);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine("Chat Server Started ...");
+            counter = 0;
+
+            while((true))
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientsList.Add(dataFromClient, clientSocket);
+
+                broadcast(dataFromClient + " Joined ", "Server", false);
+
+                Console.WriteLine(dataFromClient + " Joined Chat Room ");
+                handleClient client = new handleClient();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        public static void broadcast(string msg, string userName, bool flag)
+        {
+
+            foreach(DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadCastBytes = null;
+
+                if (flag == true)
+                {
+                    broadCastBytes = Encoding.ASCII.GetBytes(userName + " >> " + msg);
+
+                }
+                else
+                {
+                    broadCastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+
+                broadcastStream.Write(broadCastBytes, 0, broadCastBytes.Length);
+                broadcastStream.Flush();
+             }
+        }
+
+
+    }
+
+
+    public class handleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            this.clientsList = cList;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine("From Client - " + clNo + " : " + dataFromClient);
+                    rCount = Convert.ToString(requestCount);
+
+                    Program.broadcast(dataFromClient, clNo, true);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+        }
+
+    }
+
+}
